3d87fa270b12ef2454f8670756e2d856b0bb75e9
bria barry <briabarry@iMac.lan>
2021-04-08 21:44:23 -0700

    cra working

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const server = require('./api/server');
+const dotenv = require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const server = express();
+const port = process.env.PORT || 8882
+const path = require('path');
 
-server.listen(8882, () => {
-    console.log('Server is running on port 8882');
+server.use(cors());
+server.use(express.json());
+server.use(express.static(path.join(__dirname, 'client/build')));
+
+server.use('/api', (_, res) => {
+    res.json({
+        data: 'API is connected'
+    });
+})
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
